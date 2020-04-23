9d2d02bc8723a2af9361956a26671f8b937cc933
Andre Kvashuk <andremtcp2015a@gmail.com>
2020-04-23 21:50:59 +0300

    move search query to separate file

---
+++ EssentialDeveloperDemoTests/ProductionModule/RequisitesPayments/Data/SearchQuery.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  EssentialDeveloperDemo
+//
+//  Created by Andre Kvashuk on 4/23/20.
+//  Copyright © 2020 Andre Kvashuk. All rights reserved.
+//
+
+import Foundation
+
+enum SearchQuery {
+    case iban(String)
+    case taxNumber(String)
+}
